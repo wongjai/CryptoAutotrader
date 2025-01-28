802e33fc6629b3b37b7ea5a396ce748542c31733
stanislavermohin <ermohin1999@gmail.com>
2025-01-28 14:35:37 +0300

    Minor PyLint convention update

--- run.py
+++ run.py
@@ -45,57 +45,59 @@ class App:
         # Check for '<something>.env' file
         load_dotenv(dotenv_path=env_file_path)
 
-        self.EXCHANGE_NAME: str = getenv("DEFAULT_EXCHANGE_NAME")
+        self.exchange_name: str = getenv("DEFAULT_EXCHANGE_NAME")
 
-        if self.EXCHANGE_NAME in self.SUPPORTED_EXCHANGES:
-            self.EXCHANGE_API_KEY: str = getenv("EXCHANGE_API_KEY")
-            self.EXCHANGE_SECRET: str = getenv("EXCHANGE_SECRET")
-            self.EXCHANGE_PASSWORD: str = getenv("EXCHANGE_PASSPHRASE")
+        if self.exchange_name in self.SUPPORTED_EXCHANGES:
+            self.exchange_api_key: str = getenv("EXCHANGE_API_KEY")
+            self.exchange_secret: str = getenv("EXCHANGE_SECRET")
+            self.exchange_password: str = getenv("EXCHANGE_PASSPHRASE")
 
             # Exchange fee per transaction (0.1% = 0.001)
-            self.FEE: float = float(getenv("DEFAULT_EXCHANGE_FEE"))
+            self.fee: float = float(getenv("DEFAULT_EXCHANGE_FEE"))
 
         else:
             sys.exit(f"No valid exchange name provided\n"
                      f"Valid options:\n\t(`kucoin`, )"
-                     f"\nActual name provided: {self.EXCHANGE_NAME}")
+                     f"\nActual name provided: {self.exchange_name}")
 
-        self.ALGORITHM_TRUST_PERCENTAGE: float = float(getenv("ALGORITHM_TRUST_PERCENTAGE"))
-        self.DATA_VECTOR_LENGTH: int = int(getenv("DATA_VECTOR_LENGTH"))
-        self.PREMIUM: float = float(getenv("PREMIUM_OVER_EXCHANGE_FEES")) + self.FEE
-        self.MIN_TRANSACTION_VALUE_IN_BASE: float = float(getenv("MIN_TRANSACTION_VALUE_IN_BASE"))
+        self.algorithm_trust_percentage: float = float(getenv("ALGORITHM_TRUST_PERCENTAGE"))
+        self.data_vector_length: int = int(getenv("DATA_VECTOR_LENGTH"))
+        self.premium: float = float(getenv("PREMIUM_OVER_EXCHANGE_FEES")) + self.fee
+        self.min_transaction_value_in_base: float = float(getenv("MIN_TRANSACTION_VALUE_IN_BASE"))
 
         # Timeframe of data
-        self.TIMEFRAME: str = getenv("TIMEFRAME")
+        self.timeframe: str = getenv("TIMEFRAME")
 
         # Times it takes to cancel open orders
-        self.CANCEL_ORDER_LIMIT: int = int(getenv("CANCEL_ORDER_LIMIT")) or 3
+        self.cancel_order_limit: int = int(getenv("CANCEL_ORDER_LIMIT")) or 3
         self.cancel_order_counter: int = 0
 
         # Times to retry after failure
-        self.RETRIES_BEFORE_SLEEP_LIMIT: int = int(getenv("RETRIES_BEFORE_SLEEP_LIMIT")) or 4
+        self.retries_before_sleep_limit: int = int(getenv("RETRIES_BEFORE_SLEEP_LIMIT")) or 4
         self.retries_before_sleep_counter: int = 0
 
         # Optimal sleep time also works
-        self.BASE_SLEEP_TIME: int = int(getenv("BASE_SLEEP_TIME")) or \
-                                    min(max(self.DATA_VECTOR_LENGTH // 2, self.CANCEL_ORDER_LIMIT), 5) * 60
+        self.base_sleep_time: int = int(getenv("BASE_SLEEP_TIME")) or \
+                                    min(max(self.data_vector_length // 2,
+                                            self.cancel_order_limit),
+                                        5) * 60
 
         # Instantiate the Exchange class
-        self.exchange: Exchange = getattr(ccxt, self.EXCHANGE_NAME)()
+        self.exchange: Exchange = getattr(ccxt, self.exchange_name)()
 
         # Set sandbox mode to True or False (currently, True is not supported on kucoin)
         self.exchange.set_sandbox_mode(enabled=False)
 
         # Set your API keys
-        self.exchange.apiKey = self.EXCHANGE_API_KEY
-        self.exchange.secret = self.EXCHANGE_SECRET
-        self.exchange.password = self.EXCHANGE_PASSWORD  # it's called `passphrase` on KuCoin
+        self.exchange.apiKey = self.exchange_api_key
+        self.exchange.secret = self.exchange_secret
+        self.exchange.password = self.exchange_password  # it's called `passphrase` on KuCoin
 
         # Set the symbol you want to trade on KuCoin
-        self.SYMBOL: str = getenv("TRADING_PAIR")
-        pair_lst: list = self.SYMBOL.split("/")
-        self.PAIR_ITEM1: str = pair_lst[0]
-        self.PAIR_ITEM2: str = pair_lst[1]
+        self.symbol: str = getenv("TRADING_PAIR")
+        pair_lst: list = self.symbol.split("/")
+        self.pair_item1: str = pair_lst[0]
+        self.pair_item2: str = pair_lst[1]
 
         self.predict_up_or_down: Callable[[Any], str] = prediction_api
 
@@ -115,11 +117,11 @@ class App:
         """
         try:
             transaction_cost: float = round(amount * float(price), 2)
-            print(f"\t[INFO]\t⭐️ {Color.ITALIC}Trying to {buy_or_sell} {self.PAIR_ITEM1} with "
-                  f"total transaction value ≈ {transaction_cost} {self.PAIR_ITEM2}.{Color.END}")
-            if amount > self.MIN_TRANSACTION_VALUE_IN_BASE:
+            print(f"\t[INFO]\t⭐️ {Color.ITALIC}Trying to {buy_or_sell} {self.pair_item1} with "
+                  f"total transaction value ≈ {transaction_cost} {self.pair_item2}.{Color.END}")
+            if amount > self.min_transaction_value_in_base:
                 order_id: Mapping[str, Any] = self.exchange.create_order(
-                    symbol=self.SYMBOL,
+                    symbol=self.symbol,
                     type=order_type,
                     side=buy_or_sell,
                     amount=amount,
@@ -139,8 +141,8 @@ class App:
         else:
 
             print(f"[ACTION DONE]\t🤝 Place a limit {Color.BOLD}{buy_or_sell} order{Color.END}"
-                  f" of {Color.BOLD}{self.PAIR_ITEM1.lower()}{amount}{Color.END} x"
-                  f" {self.PAIR_ITEM2.lower()}{price} ≈ {self.PAIR_ITEM2.lower()}{transaction_cost}")
+                  f" of {Color.BOLD}{self.pair_item1.lower()}{amount}{Color.END} x"
+                  f" {self.pair_item2.lower()}{price} ≈ {self.pair_item2.lower()}{transaction_cost}")
             return order_id
 
     def prepare_order(self: Self) -> tuple[float | None, float | None, float | None, float | None]:
@@ -154,13 +156,13 @@ class App:
 
         # Get current balance
         balance = self.exchange.fetch_balance()
-        base_asset_balance = balance[self.PAIR_ITEM1]["free"]
-        quote_asset_balance = balance[self.PAIR_ITEM2]["free"]
-        print(f"\t[INFO]\t💰 {self.PAIR_ITEM1} balance: {base_asset_balance}")
-        print(f"\t[INFO]\t💵 {self.PAIR_ITEM2} balance: {quote_asset_balance}")
+        base_asset_balance = balance[self.pair_item1]["free"]
+        quote_asset_balance = balance[self.pair_item2]["free"]
+        print(f"\t[INFO]\t💰 {self.pair_item1} balance: {base_asset_balance}")
+        print(f"\t[INFO]\t💵 {self.pair_item2} balance: {quote_asset_balance}")
 
         try:
-            orderbook: Mapping[str, Any] = self.exchange.fetch_order_book(symbol=self.SYMBOL)
+            orderbook: Mapping[str, Any] = self.exchange.fetch_order_book(symbol=self.symbol)
             bid: Any = orderbook["bids"][0][0] if len(orderbook["bids"]) > 0 else None
             ask: Any = orderbook["asks"][0][0] if len(orderbook["asks"]) > 0 else None
             if not ask:
@@ -171,7 +173,7 @@ class App:
         except BaseException as error:
             print(f"\t[WARNING]\t...Retrying because of some error:\n\t\t{error}.\n")
             self.retries_before_sleep_counter += 1
-            if self.retries_before_sleep_counter == self.RETRIES_BEFORE_SLEEP_LIMIT:
+            if self.retries_before_sleep_counter == self.retries_before_sleep_limit:
                 self.retries_before_sleep_counter = 0
                 self.global_sleep()
             return None, None, None, None
@@ -179,24 +181,24 @@ class App:
         # Check the current bid and ask prices
         bid: float = float(bid)
         ask: float = float(ask)
-        print(f"\t[INFO]\tBid {Color.ITALIC}≈ {round(bid, 4)} {self.PAIR_ITEM2}{Color.END}"
-              f", Ask {Color.ITALIC}≈ {round(ask, 4)} {self.PAIR_ITEM2}{Color.END}\n")
+        print(f"\t[INFO]\tBid {Color.ITALIC}≈ {round(bid, 4)} {self.pair_item2}{Color.END}"
+              f", Ask {Color.ITALIC}≈ {round(ask, 4)} {self.pair_item2}{Color.END}\n")
 
         # Price is ALWAYS in quote asset (2nd item in trading pair `1st/2nd`)
         mean_price: float = (ask + bid) / 2
-        price_buy: float = mean_price * (1 - self.PREMIUM)
-        price_sell: float = mean_price * (1 + self.PREMIUM)
+        price_buy: float = mean_price * (1 - self.premium)
+        price_sell: float = mean_price * (1 + self.premium)
         # price_buy = price_sell = mean_price
         # price_buy: float = ask
         # price_sell: float = bid
 
         # Calculate how much of quote asset is about to be spent on a buy order
-        amount_to_buy_in_quote_asset: float = self.ALGORITHM_TRUST_PERCENTAGE * quote_asset_balance
+        amount_to_buy_in_quote_asset: float = self.algorithm_trust_percentage * quote_asset_balance
 
         # By CCXT rules, 'amount' variable for all '...order' methods
         # is ALWAYS in base asset (1st item in trading pair `1st/2nd`)
         amount_buy: float = amount_to_buy_in_quote_asset / price_buy
-        amount_sell: float = self.ALGORITHM_TRUST_PERCENTAGE * base_asset_balance
+        amount_sell: float = self.algorithm_trust_percentage * base_asset_balance
 
         return price_buy, price_sell, amount_buy, amount_sell
 
@@ -208,14 +210,14 @@ class App:
         :return: None
         """
 
-        print(f"\t[INFO]\t🏦 Exchange: `{self.EXCHANGE_NAME}`.\n"
+        print(f"\t[INFO]\t🏦 Exchange: `{self.exchange_name}`.\n"
               "\t[INFO]\t💼 Algorithm trust percentage (reinvestment rate): "
-              f"{Color.DARKCYAN}{self.ALGORITHM_TRUST_PERCENTAGE * 100}%{Color.END}.\n"
+              f"{Color.DARKCYAN}{self.algorithm_trust_percentage * 100}%{Color.END}.\n"
               "\t[INFO]\t📈 Algorithm premium: "
-              f"{Color.DARKCYAN}{round(self.PREMIUM * 100, 4)}%{Color.END}.\n"
+              f"{Color.DARKCYAN}{round(self.premium * 100, 4)}%{Color.END}.\n"
               "\t[INFO]\t📉 Lower limit: "
-              f"{Color.DARKCYAN}{self.MIN_TRANSACTION_VALUE_IN_BASE} {self.PAIR_ITEM1}{Color.END}.\n\n"
-              f"\t[INFO]\t🚀 Started algorithm with pair `{self.SYMBOL}`.")
+              f"{Color.DARKCYAN}{self.min_transaction_value_in_base} {self.pair_item1}{Color.END}.\n\n"
+              f"\t[INFO]\t🚀 Started algorithm with pair `{self.symbol}`.")
 
         while infinite_loop_condition:
             try:
@@ -226,14 +228,14 @@ class App:
 
                 # Check if there are any open orders
                 print("\t[INFO]\t👀 Checking for open orders for trading pair")
-                open_orders = self.exchange.fetch_open_orders(self.SYMBOL)
+                open_orders = self.exchange.fetch_open_orders(self.symbol)
                 if not open_orders:
                     print("\t[INFO]\t🟢 No open orders.")
 
                     data: Any = self.exchange.fetch_ohlcv(
-                        self.SYMBOL, self.TIMEFRAME, limit=self.DATA_VECTOR_LENGTH)
+                        self.symbol, self.timeframe, limit=self.data_vector_length)
                     print("\t[INFO]\t📊 Got data: "
-                          f"({self.DATA_VECTOR_LENGTH} x {self.TIMEFRAME}).")
+                          f"({self.data_vector_length} x {self.timeframe}).")
 
                     try:
                         # Check if it is bullish up or bearish down before buying
@@ -245,14 +247,14 @@ class App:
                         else:
                             print("\t[AI]\t🤖 Could not get prediction.")
                             self.retries_before_sleep_counter += 1
-                            if self.retries_before_sleep_counter == self.RETRIES_BEFORE_SLEEP_LIMIT:
+                            if self.retries_before_sleep_counter == self.retries_before_sleep_limit:
                                 self.retries_before_sleep_counter = 0
                                 self.global_sleep()
                             continue
 
                         # If bullish
                         if prediction_main == "up":
-                            print(f"\t[AI]\t🤖 Is {Color.GREEN}bullish{Color.END} on {self.PAIR_ITEM1}.")
+                            print(f"\t[AI]\t🤖 Is {Color.GREEN}bullish{Color.END} on {self.pair_item1}.")
 
                             price_buy, _, amount_buy, _ = self.prepare_order()
                             if amount_buy is None:
@@ -268,7 +270,7 @@ class App:
 
                         # If bearish
                         elif prediction_main == "down":
-                            print(f"\t[AI]\t🤖 Is {Color.RED}bearish{Color.END} on {self.PAIR_ITEM1}.")
+                            print(f"\t[AI]\t🤖 Is {Color.RED}bearish{Color.END} on {self.pair_item1}.")
 
                             _, price_sell, _, amount_sell = self.prepare_order()
                             if amount_sell is None:
@@ -284,7 +286,7 @@ class App:
 
                         # If indecisive
                         elif prediction_main == "hold":
-                            print(f"\t[AI]\t🤖 Is {Color.PURPLE}hold{Color.END} on {self.PAIR_ITEM1}.")
+                            print(f"\t[AI]\t🤖 Is {Color.PURPLE}hold{Color.END} on {self.pair_item1}.")
                             print("\t[INFO]\t😎 Doing nothing.")
 
                         else:
@@ -298,7 +300,7 @@ class App:
 
                     except BaseException as error:
                         self.retries_before_sleep_counter += 1
-                        if self.retries_before_sleep_counter == self.RETRIES_BEFORE_SLEEP_LIMIT:
+                        if self.retries_before_sleep_counter == self.retries_before_sleep_limit:
                             self.retries_before_sleep_counter = 0
                             self.default_sleep_message(error, "ProbablyAIButCouldBeAnything")
                             self.global_sleep()
@@ -311,7 +313,7 @@ class App:
                     print(f"\t[INFO]\t💪🏻 {Color.ITALIC}Current open orders counter:{Color.END}"
                           f" {Color.BOLD}{Color.DARKCYAN}{self.cancel_order_counter}{Color.END}.")
 
-                    if self.cancel_order_counter == self.CANCEL_ORDER_LIMIT:
+                    if self.cancel_order_counter == self.cancel_order_limit:
                         self.cancel_order_counter = 0
                         for order in open_orders:
                             order_id_to_cancel: str = order.get("id")
@@ -344,11 +346,11 @@ class App:
         print("[END]\t👋🏻 END `main` module.")
 
     def global_sleep(self: Self) -> None:
-        print(f"\t[INFO]\t🙈 Pause for {self.BASE_SLEEP_TIME} seconds.")
-        sleep(self.BASE_SLEEP_TIME)
+        print(f"\t[INFO]\t🙈 Pause for {self.base_sleep_time} seconds.")
+        sleep(self.base_sleep_time)
 
     def default_sleep_message(self: Self, error: Any, tag: str) -> None:
-        print(f"\t[ERROR]\t🙈 Retrying after sleep ({self.BASE_SLEEP_TIME} seconds). "
+        print(f"\t[ERROR]\t🙈 Retrying after sleep ({self.base_sleep_time} seconds). "
               f"{tag} exception:\n\t\t{error}.\n")
 
 
